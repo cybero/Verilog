cccd2849efe8eb0f759290f776013bb538bbddc9
cybero <cybero@hotmail.es>
2017-06-28 14:30:32 +0200

    modification

--- src/Frequency Divider 1/top_TB.v
+++ src/Frequency Divider 1/top_TB.v
@@ -1,29 +1,30 @@
 `timescale 1ns / 1ps
 
 module top_TB();
-    reg clk_in, nreset;
-    wire clk_out;
-    Freq_Divider  #(
+  reg clk_in, nreset;
+  wire clk_out;
+  
+  Freq_Divider  #(
     .sys_clk(50000000),
     .desired_clk(25000000)) 
-    U1 (.clk_in(clk_in), .clk_out(clk_out),.nreset(nreset));
-initial
-begin
-$monitor($time, " clk = %b, rst = %b, out_clk = %b",clk_in,nreset,clk_out);
-clk_in=0;
-nreset=0;
-#20 nreset=1;
-               #500 $finish;
+  U1 (.clk_in(clk_in), .clk_out(clk_out),.nreset(nreset));
+  
+  initial
+    begin
+      $monitor($time, " clk = %b, rst = %b, out_clk = %b",clk_in,nreset,clk_out);
+      clk_in=0;
+      nreset=0;
+      #20 nreset=1;
+      #500 $finish;
+    end
 
-end
+  always 
+    #5  clk_in =  ! clk_in; 
 
-   always 
-       #5  clk_in =  ! clk_in; 
-
-initial
-begin
-     $dumpfile ("frequencydiv.vcd");
-     $dumpvars (0,top_TB);
-end
+  initial
+    begin
+      $dumpfile ("frequencydiv.vcd");
+      $dumpvars (0,top_TB);
+    end
 
 endmodule
